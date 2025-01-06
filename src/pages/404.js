78e3f2b1fee52296a793a8320ae54584d0c5eb4d
Johan Lorrez <johan@lorrez.be>
2025-01-06 16:02:33 +0100

    End of day commit

--- src/pages/404.js
+++ src/pages/404.js
@@ -9,7 +9,7 @@ const NotFoundPage = () => {
         <div className="container mx-auto">
           <h1 className="text-5xl font-bold text-gray-800 mb-6">404</h1>
           <p className="text-lg text-gray-600 mb-8">
-            Sorry, the page you are looking for does not exist.
+            Sorry, the page you are looking for does not exist here.
           </p>
           <Link
             to="/"
