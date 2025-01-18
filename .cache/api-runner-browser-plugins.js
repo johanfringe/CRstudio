1152952d57fc90e82d05359334c3b9129e64e8a9
Johan Lorrez <johan@lorrez.be>
2025-01-18 09:43:24 +0100

    End of day commit

--- .cache/api-runner-browser-plugins.js
+++ .cache/api-runner-browser-plugins.js
@@ -1,6 +1,6 @@
 module.exports = [{
       plugin: require('../node_modules/gatsby-plugin-react-i18next/gatsby-browser.js'),
-      options: {"plugins":[],"localeJsonSourceName":"locales","languages":["en","fr","nl","zu"],"defaultLanguage":"zu","siteUrl":"http://localhost:8000","i18nextOptions":{"interpolation":{"escapeValue":false},"detection":{"order":["path","htmlTag","cookie","navigator"],"caches":["cookie"]},"fallbackLng":"zu","backend":{"loadPath":"/Users/johan/CRstudio/CRstudio/src/locales/{{lng}}/{{ns}}.json"}},"pages":[{"matchPath":"/:lang/:rest*","getLanguageFromPath":true}]},
+      options: {"plugins":[],"localeJsonSourceName":"locales","languages":["en","fr","nl","zu"],"defaultLanguage":"zu","siteUrl":"https://crstudio.online","i18nextOptions":{"interpolation":{"escapeValue":false},"detection":{"order":["path","htmlTag","cookie","navigator"],"caches":["cookie"]},"fallbackLng":"zu","backend":{"loadPath":"/Users/johan/CRstudio/CRstudio/src/locales/{{lng}}/{{ns}}.json"}},"pages":[{"matchPath":"/:lang/:rest*","getLanguageFromPath":true}]},
     },{
       plugin: require('../node_modules/gatsby-plugin-manifest/gatsby-browser.js'),
       options: {"plugins":[],"name":"Catalogue Raisonné","short_name":"CR","start_url":"/","background_color":"#ffffff","theme_color":"#663399","display":"standalone","icon":"static/icons/favicon-512x512.png","legacy":true,"theme_color_in_head":true,"cache_busting_mode":"query","crossOrigin":"anonymous","include_favicon":true,"cacheDigest":"25fcdfc9b3ec1ab58885bcc31da4342e"},
