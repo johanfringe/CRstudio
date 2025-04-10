5122f0aa3fd0503fa6b95547bea3f24583a901ff
Johan Lorrez <johan@lorrez.be>
2025-04-10 17:36:07 +0200

    ✅ Final test: working husky, lint-staged, prettier, eslint

---
+++ .eslintrc.js
@@ -0,0 +1,41 @@
+module.exports = {
+    root: true,
+    env: {
+      browser: true,
+      es2021: true,
+      node: true,
+    },
+    parserOptions: {
+      ecmaVersion: 2021,
+      sourceType: "module",
+      ecmaFeatures: {
+        jsx: true,
+      },
+    },
+    extends: [
+      "eslint:recommended",
+      "plugin:react/recommended",
+      "plugin:jsx-a11y/recommended",
+      "plugin:react-hooks/recommended",
+      "plugin:prettier/recommended",
+      "plugin:i18next/recommended",
+    ],
+    plugins: ["react", "jsx-a11y", "react-hooks", "prettier", "i18next"],
+    rules: {
+      "react/react-in-jsx-scope": "off",
+      "react/prop-types": "off",
+      "prettier/prettier": "error",
+      "i18next/no-literal-string": [
+        "warn",
+        {
+          markupOnly: true,
+          ignoreAttribute: ["id", "name", "type", "htmlFor", "data-testid"],
+        },
+      ],
+    },
+    settings: {
+      react: {
+        version: "detect",
+      },
+    },
+  };
---
+++ lint-staged.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    "src/**/*.{js,jsx,ts,tsx}": ["eslint --fix", "prettier --write"],
+    "*.{json,css,md}": ["prettier --write"]
+  };
+  
