b2531b97b8f41f89699cac14d0074648de244ca6
Johan Lorrez <johan@lorrez.be>
2025-05-17 14:25:46 +0200

    Add initial test for checkHIBPPassword to fix CI failure

--- src/utils/__tests__/checkHIBPPassword.test.js
+++ src/utils/__tests__/checkHIBPPassword.test.js
@@ -1,2 +1,12 @@
 // __tests__/checkHIBPPassword.test.js
 
+// src/utils/__tests__/checkHIBPPassword.test.js
+
+import { checkHIBPPassword } from '../checkHIBPPassword';
+
+describe('checkHIBPPassword', () => {
+  test('moet een geldige string aanvaarden', async () => {
+    const result = await checkHIBPPassword('superveilig123');
+    expect(typeof result).toBe('boolean');
+  });
+});
