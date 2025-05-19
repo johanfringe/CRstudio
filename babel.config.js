f2d21f21e3bb0854e738398c0c48cda313ae19bb
Johan Lorrez <johan@lorrez.be>
2025-05-19 13:38:41 +0200

     End of day commit

--- babel.config.js
+++ babel.config.js
@@ -1,4 +1,5 @@
-module.exports = {
-    presets: ['@babel/preset-env', '@babel/preset-react'],
-  };
-  
+// babel.config.js (in ESM) :
+
+export default {
+  presets: ['@babel/preset-env', '@babel/preset-react'],
+};
